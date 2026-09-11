7d58f00af372707908f72bac4aad0ec371d4cac0
maxtyson123 <98maxt98@gmail.com>
2026-09-11 14:04:04 +1200

    Remove Build Artifacts

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,12 @@
-int x = 4;
-int y = 3;
+int x = 42;
+int y = 24;
 
 int main() {
 	int a = 2;
 	int b = 4;
 
-	// Without parentheses:     x + y  *  a + b   =  4 + (3 * 2) + 4    =    14    = 14
-	//   With  parentheses:    (x + y) * (a + b)  =  (4 + 3) * (2 + 4)  =  7 * 6  =  42
-
-	return (x + y) * (a + b);
+	if (a == b)
+		return x;
+	else
+		return y;
 }
